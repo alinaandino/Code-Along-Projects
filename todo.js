77680bc9fc39d89b5bbda21ee654685d7c812616
Alina Andino <109756284+alinaandino@users.noreply.github.com>
2022-10-12 10:23:53 -0500

    Add files via upload
    Finished project

--- todo.js
+++ todo.js
@@ -1,7 +1,11 @@
+//There is a way to keep my tasks on the page with localStorage but TBD on that one. Need
+//to do more research on that
+
 window.addEventListener("load", ()=> {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
+
     
     form.addEventListener("submit", (e) =>{
         e.preventDefault();
@@ -67,6 +71,9 @@ window.addEventListener("load", ()=> {
                 // console.log("Save"); just a check
             }
         });
+        task_delete_el.addEventListener('click',() =>{
+            list_el.removeChild(task_el);
+        })
     })
 }) 
 
